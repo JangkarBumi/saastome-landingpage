e8973c057c058c1c22fc98c19bad66c6383c0650
Zeha Irawan <jangkaringbumi@gmail.com>
2021-01-21 05:55:08 +0700

    delete tome1

--- pages/tome2.js
+++ pages/tome2.js
@@ -1,5 +1,43 @@
 const Tome2 = () => {
-  return <h1>Tome2</h1>;
+  return (
+    <div>
+      {/*Navbar*/}
+
+      {/* Hero Section*/}
+      <div className="hero">
+        <div>
+          <h1>Want anything to be easy with LaslesVPN.</h1>
+          <p>
+            Provide a network for all your needs with ease and fun using
+            LaslesVPN discover interesting features from us.
+          </p>
+          <button>Get Started</button>
+        </div>
+        <img src="" alt="" />
+      </div>
+
+      {/* Hero Section*/}
+      <div>
+        <img src="" alt="" />
+        <div>
+          <h1>We Provide Many Features You Can Use</h1>
+          <p>
+            You can explore the features that we provide with fun and have their
+            own functions each feature.
+          </p>
+          <ul>
+            <li>Powerful online protection.</li>
+            <li>Internet without borders.</li>
+            <li>Supercharged VPN.</li>
+            <li>No specific time limits.</li>
+          </ul>
+        </div>
+      </div>
+      {/* Plan Section*/}
+      {/* Testimony Section*/}
+      {/* Footer Section*/}
+    </div>
+  );
 };
 
 export default Tome2;
